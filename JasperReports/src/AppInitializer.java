94fbc1e4601e1ef920a18d4dda94ef82918ae55c
sanuv9683 <sanuv9683@gmail.com>
2022-03-10 11:33:49 +0530

    Initial Commit

---
+++ JasperReports/src/AppInitializer.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * @author : Sanu Vithanage
+ * @since : 0.1.0
+ **/
+public class AppInitializer extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        AnchorPane pane = FXMLLoader.load(this.getClass().getResource("/view/JasperReports.fxml"));
+        Scene scene= new Scene(pane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
---
+++ JasperReports/src/controller/JasperReportController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * @author : Sanu Vithanage
+ * @since : 0.1.0
+ **/
+public class JasperReportController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
